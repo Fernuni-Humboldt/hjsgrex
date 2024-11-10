143d0bb610a83390f07b1d1d4fabfa8ebff54189
MaxTiger <ichbincool42342@gmx.de>
2024-11-10 11:09:21 +0100

    Update cookieconsent-config.js

--- cookieconsent-config.js
+++ cookieconsent-config.js
@@ -48,31 +48,33 @@ Wir verwenden Cookies und andere Technologien auf unserer Website. Einige von ih
                     sections: [
                         {
                             title: "Verwendung von Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                            description: "Wir benötigen Ihre Zustimmung, bevor Sie auf unserer Website fortfahren können.
+Wenn Sie unter 16 Jahre alt sind und optionalen Diensten zustimmen möchten, müssen Sie Ihre Erziehungsberechtigten um Erlaubnis fragen.
+Wir verwenden Cookies und andere Technologien auf unserer Website. Einige von ihnen sind essenziell, während andere uns helfen, diese Website und Ihre Erfahrung zu verbessern. Personenbezogene Daten können verarbeitet werden (z. B. IP-Adressen), beispielsweise für personalisierte Anzeigen und Inhalte oder Anzeigen- und Inhaltsmessung."
                         },
                         {
                             title: "Streng Notwendige Cookies <span class=\"pm__badge\">Immer Aktiviert</span>",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            description: "Diese Cookies sind erforderlich, damit unsere Website korrekt funktioniert und grundlegende Funktionen, wie die Seitennavigation, bereitstellt. Sie können nicht deaktiviert werden.",
                             linkedCategory: "necessary"
                         },
                         {
                             title: "Funktionalitäts Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            description: "Diese Cookies ermöglichen zusätzliche Funktionen, wie das Speichern Ihrer Einstellungen und Präferenzen, um Ihre Erfahrung zu personalisieren.",
                             linkedCategory: "functionality"
                         },
                         {
                             title: "Analytische Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            description: "Sie helfen uns zu verstehen, wie unsere Besucher die Website nutzen. So können wir die Benutzerfreundlichkeit verbessern, indem wir Informationen über die Nutzung sammeln und analysieren.",
                             linkedCategory: "analytics"
                         },
                         {
                             title: "Werbung Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            description: "Diese Cookies helfen uns, Ihnen relevante Werbeinhalte anzuzeigen, basierend auf Ihren Interessen und Ihrem Surfverhalten auf unserer Seite.",
                             linkedCategory: "marketing"
                         },
                         {
                             title: "Weitere Informationen",
-                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                            description: "Weitere Informationen zu den verwendeten Cookies und deren Zweck finden Sie in unserer Datenschutzerklärung."
                         }
                     ]
                 }

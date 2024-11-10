bc95dcb28867a16945f7ab6c003dbf3386f46d67
MaxTiger <ichbincool42342@gmx.de>
2024-11-10 10:56:32 +0100

    Add files via upload

---
+++ cookieconsent-config.js
@@ -0,0 +1,80 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js';
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "bar inline",
+            position: "bottom",
+            equalWeightButtons: true,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "bar wide",
+            position: "right",
+            equalWeightButtons: true,
+            flipButtons: false
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        },
+        functionality: {},
+        analytics: {},
+        marketing: {}
+    },
+    language: {
+        default: "de",
+        autoDetect: "browser",
+        translations: {
+            de: {
+                consentModal: {
+                    title: "Hallo Reisende, es ist Kekszeit!",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    acceptAllBtn: "Alle akzeptieren",
+                    acceptNecessaryBtn: "Alle ablehnen",
+                    showPreferencesBtn: "Einstellungen verwalten",
+                    footer: "<a href=\"#link\">Datenschutz</a>\n<a href=\"#link\">Bedingungen und Konditionen</a>"
+                },
+                preferencesModal: {
+                    title: "Präferenzen für die Zustimmung",
+                    acceptAllBtn: "Alle akzeptieren",
+                    acceptNecessaryBtn: "Alle ablehnen",
+                    savePreferencesBtn: "Einstellungen speichern",
+                    closeIconLabel: "Modal schließen",
+                    serviceCounterLabel: "Dienstleistungen",
+                    sections: [
+                        {
+                            title: "Verwendung von Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        },
+                        {
+                            title: "Streng Notwendige Cookies <span class=\"pm__badge\">Immer Aktiviert</span>",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Funktionalitäts Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "functionality"
+                        },
+                        {
+                            title: "Analytische Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "Werbung Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "marketing"
+                        },
+                        {
+                            title: "Weitere Informationen",
+                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
